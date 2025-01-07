3196a54b5470f9e9ff0bbf02053a6b1a26c610f3
Villarma Ryan <ryanvillarma148@gmail.com>
2025-01-08 01:26:06 +0800

    First commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// Unsplash Links
+const photos = [
+  "https://images.unsplash.com/photo-1547241850-eb8a8754f95c?q=80&w=1527&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_photo-1669863547357-b7d064cedaac?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1504608524841-42fe6f032b4b?q=80&w=1530&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_photo-1676218968741-8179dd7e533f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1542971503-8b6a4dc27674?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1461566978495-8c8f681e1235?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1520262494112-9fe481d36ec3?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_photo-1675342786681-e33a19414cfd?q=80&w=1490&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_photo-1727730047398-49766e915c1d?q=80&w=1512&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1736192252876-e086cd9fa39b?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1735760672876-1c6f801f6436?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_photo-1736167753494-13b26720e9fe?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+
+const GALLERY_CONTAINER = document.querySelector(".gallery-container");
+const SELECT = document.querySelector("select");
+
+// Default number of columns
+let columns = 4; 
+
+/* ============= ~ EVENTS ~ ============= */
+
+window.addEventListener("DOMContentLoaded", () => render());
+
+SELECT.addEventListener("change", (e) => {
+  columns = +e.target.value;
+  return render();
+});
+
+/* =========== ~ END EVENTS ~ ========== */
+
+
+function render() {
+    
+    // Remove first all the contents before rerender
+    GALLERY_CONTAINER.replaceChildren();
+
+    // Create fixed columns first
+    const columnDivs = Array.from({ length: columns }, () => {
+        const div = document.createElement("div");
+        div.className = "column flex flex-col space-y-3";
+        GALLERY_CONTAINER.appendChild(div);
+        return div;
+    });
+
+    // Distribute photos among columns
+    photos.forEach((photo, index) => {
+        const img = document.createElement("img");
+        img.src = photo;
+        img.className = "rounded-lg object-cover";
+        img.setAttribute("loading", "lazy");
+        img.setAttribute("alt", `photo-${index}`);
+        columnDivs[index % columns].appendChild(img);
+    });
+}
